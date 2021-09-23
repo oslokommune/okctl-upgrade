2e3ccf8835f1f53843601d58b36a4a3fb995d4d1
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2021-09-23 09:05:14 +0200

    ✅ Describe flags

--- template/main.go
+++ template/main.go
@@ -20,6 +20,7 @@ type cmdFlags struct {
 
 func buildRootCommand() *cobra.Command {
 	flags := cmdFlags{}
+
 	var context Context
 
 	cmd := &cobra.Command{
@@ -41,9 +42,20 @@ func buildRootCommand() *cobra.Command {
 		},
 	}
 
-	cmd.PersistentFlags().BoolVarP(&flags.debug, "debug", "d", false, "Set to true to enable debug output.")
+	/*
+	 * Flags supported. Expected behavior is as following:
+	 *
+	 * --debug:		Outputs extra output for debugging.
+	 *
+	 * --dry-run: 	If set to true, the upgrade will not make any changes, but only print what would be done, as if
+	 * 				running a simulation.
+	 *				If set to false, the upgrade will make actual changes.
+	 *
+	 * --confirm:	Skips all confirmation prompts, if any.
+	 */
+	cmd.PersistentFlags().BoolVarP(&flags.debug, "debug", "d", false, "Set this to enable debug output.")
 	cmd.PersistentFlags().BoolVarP(&flags.debug, "dry-run", "n", true, "Don't actually do any changes, just show what would be done.")
-	cmd.PersistentFlags().BoolVarP(&flags.force, "confirm", "c", false, "Set to true to skip confirmation prompts.")
+	cmd.PersistentFlags().BoolVarP(&flags.force, "confirm", "c", false, "Set this to skip confirmation prompts.")
 
 	return cmd
 }
