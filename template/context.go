aed1a7d8b1918e62ca5481b16fabf178e815ace6
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2022-01-27 09:49:47 +0100

    Update template

--- template/context.go
+++ template/context.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"github.com/oslokommune/okctl-upgrade/template/pkg/logger"
+	"github.com/oslokommune/okctl-upgrade/template/pkg/lib/cmdflags"
+	"github.com/oslokommune/okctl-upgrade/template/pkg/lib/logger"
 )
 
 type Context struct {
 	logger logger.Logger
 }
 
-func newContext(flags cmdFlags) Context {
+func newContext(flags cmdflags.Flags) Context {
 	var level logger.Level
-	if flags.debug {
+	if flags.Debug {
 		level = logger.Debug
 	} else {
 		level = logger.Info
