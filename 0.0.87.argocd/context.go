41b08d9367035b833659c675552ce54a0accc265
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2022-01-27 09:27:40 +0100

    Update ArgoCD upgrade

---
+++ 0.0.87.argocd/context.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/oslokommune/okctl-upgrade/0.0.87.argocd/pkg/lib/cmdflags"
+	"github.com/oslokommune/okctl-upgrade/0.0.87.argocd/pkg/lib/logger"
+)
+
+type Context struct {
+	log logger.Logger
+}
+
+func newContext(flags cmdflags.Flags) Context {
+	var level logger.Level
+	if flags.Debug {
+		level = logger.Debug
+	} else {
+		level = logger.Info
+	}
+
+	return Context{
+		log: logger.New(level),
+	}
+}
