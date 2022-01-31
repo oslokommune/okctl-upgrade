0ce964604bfd2eb1433720b1ca4abf16865a2b24
Julius Pedersen <deifyed@tutanota.com>
2022-01-31 13:29:43 +0100

    👌 Fix all module references

--- 0.0.88.activate-argo-app-sync/upgrade.go
+++ 0.0.88.activate-argo-app-sync/upgrade.go
@@ -1,21 +1,6 @@
 package main
 
-import (
-	"github.com/oslokommune/okctl-upgrade/template/pkg/somecomponent"
-)
-
-func upgrade(context Context, flags cmdFlags) error {
-	opts := somecomponent.Opts{
-		DryRun:  flags.dryRun,
-		Confirm: flags.confirm,
-	}
-
-	c := somecomponent.New(context.logger, opts)
-
-	err := c.Upgrade()
-	if err != nil {
-		return err
-	}
+func upgrade(_ Context, _ cmdFlags) error {
 
 	return nil
 }
