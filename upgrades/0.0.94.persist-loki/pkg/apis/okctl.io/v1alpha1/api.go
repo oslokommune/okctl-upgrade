7b983dda05ea0ec92eafee28e4b11eebf8e129a7
Julius Pedersen <deifyed@tutanota.com>
2022-05-06 08:21:00 +0200

    ✅ Add cluster manifest handling

---
+++ upgrades/0.0.94.persist-loki/pkg/apis/okctl.io/v1alpha1/api.go
@@ -0,0 +1,18 @@
+package v1alpha1
+
+import (
+	"fmt"
+	"sigs.k8s.io/yaml"
+)
+
+// Parse knows how to convert bytes into a Cluster
+func Parse(raw []byte) (Cluster, error) {
+	clusterManifest := NewCluster()
+
+	err := yaml.Unmarshal(raw, &clusterManifest)
+	if err != nil {
+		return Cluster{}, fmt.Errorf("unmarshalling: %w", err)
+	}
+
+	return clusterManifest, nil
+}
--- upgrades/0.0.94.persist-loki/upgrade.go
+++ upgrades/0.0.94.persist-loki/upgrade.go
@@ -3,29 +3,39 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.94.persist-loki/pkg/apis/okctl.io/v1alpha1"
 	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.94.persist-loki/pkg/eksctl"
 	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.94.persist-loki/pkg/lib/cmdflags"
 	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.94.persist-loki/pkg/policies"
 	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.94.persist-loki/pkg/s3"
 	"github.com/spf13/afero"
+	"os"
 )
 
 func upgrade(ctx context.Context, _ cmdflags.Flags) error {
 	fs := &afero.Afero{Fs: afero.NewOsFs()}
-	clusterName := "upgrade-test"
-	awsAccountID := ""
-	awsRegion := "eu-west-1"
+	clusterManifestPath := os.Getenv("OKCTL_CLUSTER_DECLARATION")
 
-	bucketName := fmt.Sprintf("okctl-%s-loki", clusterName)
+	rawManifest, err := fs.ReadFile(clusterManifestPath)
+	if err != nil {
+		return fmt.Errorf("reading manifest: %w", err)
+	}
 
-	arn, err := s3.CreateBucket(ctx, clusterName, bucketName)
+	clusterManifest, err := v1alpha1.Parse(rawManifest)
+	if err != nil {
+		return fmt.Errorf("parsing manifest: %w", err)
+	}
+
+	bucketName := fmt.Sprintf("okctl-%s-loki", clusterManifest.Metadata.Name)
+
+	arn, err := s3.CreateBucket(ctx, clusterManifest.Metadata.Name, bucketName)
 	if err != nil {
 		return fmt.Errorf("creating bucket: %w", err)
 	}
 
 	fmt.Printf("S3 ARN: %s\n", arn)
 
-	s3PolicyARN, err := policies.CreateS3BucketPolicy(ctx, clusterName, arn)
+	s3PolicyARN, err := policies.CreateS3BucketPolicy(ctx, clusterManifest.Metadata.Name, arn)
 	if err != nil {
 		return fmt.Errorf("creating bucket policy: %w", err)
 	}
@@ -34,9 +44,9 @@ func upgrade(ctx context.Context, _ cmdflags.Flags) error {
 
 	dynamoDBPolicyARN, err := policies.CreateDynamoDBPolicy(
 		ctx,
-		awsAccountID,
-		awsRegion,
-		clusterName,
+		clusterManifest.Metadata.AccountID,
+		clusterManifest.Metadata.Region,
+		clusterManifest.Metadata.Name,
 	)
 	if err != nil {
 		return fmt.Errorf("creating Dynamo DB policy: %w", err)
@@ -44,7 +54,12 @@ func upgrade(ctx context.Context, _ cmdflags.Flags) error {
 
 	fmt.Printf("DynamoDB policy ARN: %s\n", dynamoDBPolicyARN)
 
-	err = eksctl.CreateServiceUser(fs, clusterName, "loki", []string{s3PolicyARN, dynamoDBPolicyARN})
+	err = eksctl.CreateServiceUser(
+		fs,
+		clusterManifest.Metadata.Name,
+		"loki",
+		[]string{s3PolicyARN, dynamoDBPolicyARN},
+	)
 	if err != nil {
 		return fmt.Errorf("creating service user: %w", err)
 	}
