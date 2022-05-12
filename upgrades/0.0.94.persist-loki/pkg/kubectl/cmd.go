7e0711fc7bb3dcc48fe8dc85b7c7a293cf2ac2c9
Julius Pedersen <deifyed@tutanota.com>
2022-05-12 08:38:39 +0200

    ✅ Add env handling for kubectl

--- upgrades/0.0.94.persist-loki/pkg/kubectl/cmd.go
+++ upgrades/0.0.94.persist-loki/pkg/kubectl/cmd.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
+	"os"
 	"os/exec"
 	"path"
 	"runtime"
+	"strings"
 
 	"github.com/Masterminds/semver"
 	"github.com/spf13/afero"
@@ -46,21 +48,78 @@ func acquireBinaryPath(Fs *afero.Afero, homeDirFn func() (string, error)) (strin
 	return path.Join(binaryDir, latest.String(), runtime.GOOS, runtime.GOARCH, defaultBinaryName), nil
 }
 
-func runCommand(binaryPath string, args ...string) (io.Reader, error) {
-	cmd := exec.Command(binaryPath, args...) //nolint:gosec
+func runCommand(fs *afero.Afero, kubeconfigPath string, args ...string) (io.Reader, error) {
+	cmd := exec.Command(defaultBinaryName, args...) //nolint:gosec
 
 	stderr := bytes.Buffer{}
 	stdout := bytes.Buffer{}
 
+	env, err := generateEnv(fs, kubeconfigPath)
+	if err != nil {
+		return nil, fmt.Errorf("generating env: %w", err)
+	}
+
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
+	cmd.Env = env
 
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", stderr.String(), err)
 	}
 
 	return &stdout, nil
+}
+
+func generateEnv(fs *afero.Afero, kubeconfigPath string) ([]string, error) {
+	envMap := make(map[string]string)
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, fmt.Errorf("acquiring home directory: %w", err)
+	}
+
+	binariesDir := path.Join(homeDir, defaultOkctlConfigDirName, defaultOkctlBinariesDirName)
+	toolDirectories := make([]string, 0)
+
+	for _, tool := range []string{defaultBinaryName, defaultAWSIAMAuthenticatorName} {
+		toolBaseDir := path.Join(binariesDir, tool)
+
+		versions, err := gatherVersions(fs, toolBaseDir)
+		if err != nil {
+			return nil, fmt.Errorf("gathering versions: %w", err)
+		}
+
+		latestVersion := getLatestVersion(versions)
+
+		toolDirectories = append(toolDirectories, path.Join(
+			toolBaseDir,
+			latestVersion.String(),
+			runtime.GOOS,
+			defaultArch,
+			tool,
+		))
+	}
+
+	envMap["HOME"] = homeDir
+	envMap["KUBECONFIG"] = kubeconfigPath
+	envMap["PATH"] = strings.Join(toolDirectories, ":")
+
+	return envAsArray(envMap), nil
+}
+
+// envAsArray converts a map to a string array of KEY=VALUE pairs
+func envAsArray(m map[string]string) []string {
+	result := make([]string, len(m))
+	index := 0
+
+	for key, value := range m {
+		result[index] = fmt.Sprintf("%s=%s", key, value)
+
+		index++
+	}
+
+	return result
 }
 
 func gatherVersions(Fs *afero.Afero, baseDir string) (map[semver.Version]interface{}, error) {
@@ -92,4 +151,16 @@ func gatherVersions(Fs *afero.Afero, baseDir string) (map[semver.Version]interfa
 	}
 
 	return versions, nil
+}
+
+func getLatestVersion(versions map[semver.Version]interface{}) semver.Version {
+	var latest semver.Version
+
+	for version := range versions {
+		if version.GreaterThan(&latest) {
+			latest = version
+		}
+	}
+
+	return latest
 }
