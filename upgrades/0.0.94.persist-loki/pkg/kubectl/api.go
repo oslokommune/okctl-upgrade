6fe6724eb75b90b07db67b4362c1179805e59c4c
Julius Pedersen <deifyed@tutanota.com>
2022-05-20 08:39:12 +0200

    ✅ Add functionality for handling AZ problem

--- upgrades/0.0.94.persist-loki/pkg/kubectl/api.go
+++ upgrades/0.0.94.persist-loki/pkg/kubectl/api.go
@@ -108,3 +108,73 @@ func RestartLoki(fs *afero.Afero) error {
 
 	return nil
 }
+
+func HasVolumeClaim(fs *afero.Afero, claimName string) (bool, error) {
+	result, err := runCommand(fs,
+		"--namespace", defaultMonitoringNamespace,
+		"--output", "json",
+		"get", persistentVolumeClaimResourceKind, claimName,
+	)
+	if err != nil {
+		return false, fmt.Errorf("acquiring volume claims: %w", err)
+	}
+
+	resultAsBytes, err := io.ReadAll(result)
+	if err != nil {
+		return false, fmt.Errorf("buffering: %w", err)
+	}
+
+	var itemIterator iterator
+
+	err = json.Unmarshal(resultAsBytes, &itemIterator)
+	if err != nil {
+		return false, fmt.Errorf("unmarshalling: %w", err)
+	}
+
+	for _, item := range itemIterator.Items {
+		if item.Metadata.Name == claimName {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func GetVolumeClaimAZ(fs *afero.Afero, claimName string) (string, error) {
+	volumeID, err := getVolumeID(fs, claimName)
+	if err != nil {
+		return "", fmt.Errorf("acquiring volume ID: %w", err)
+	}
+
+	zone, err := getVolumeZone(fs, volumeID)
+	if err != nil {
+		return "", fmt.Errorf("acquiring volume zone: %w", err)
+	}
+
+	return zone, nil
+}
+
+func AddNodeSelector(fs *afero.Afero, statefulsetName string, key string, value string) error {
+	patch := jsp.New().Add(jsp.Operation{
+		Type:  jsp.OperationTypeAdd,
+		Path:  fmt.Sprintf("/spec/template/spec/nodeSelector/%s", key),
+		Value: value,
+	})
+
+	rawPatch, err := patch.MarshalJSON()
+	if err != nil {
+		return fmt.Errorf("marshalling: %w", err)
+	}
+
+	_, err = runCommand(fs,
+		"--namespace", defaultMonitoringNamespace,
+		"--type=json",
+		"patch", statefulSetResourceKind, statefulsetName,
+		"--patch", string(rawPatch),
+	)
+	if err != nil {
+		return fmt.Errorf("patching: %w", err)
+	}
+
+	return nil
+}
--- upgrades/0.0.94.persist-loki/pkg/loki/api.go
+++ upgrades/0.0.94.persist-loki/pkg/loki/api.go
@@ -4,12 +4,19 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/spf13/afero"
+
 	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.94.persist-loki/pkg/lib/context"
 
 	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.94.persist-loki/pkg/kubectl"
 )
 
 func AddPersistence(ctx context.Context, region string, clusterName string, bucketName string) error {
+	err := ensureNodeSelector(ctx.Fs, "loki", "loki")
+	if err != nil {
+		return fmt.Errorf("ensuring defined nodeSelector: %w", err)
+	}
+
 	// The new config should be active the next calendar preventing issues with index tables spanning two configurations
 	from := time.Now().Add(24 * time.Hour)
 
@@ -52,6 +59,29 @@ func AddPersistence(ctx context.Context, region string, clusterName string, buck
 	err = kubectl.RestartLoki(ctx.Fs)
 	if err != nil {
 		return fmt.Errorf("restarting Loki: %w", err)
+	}
+
+	return nil
+}
+
+func ensureNodeSelector(fs *afero.Afero, volumeClaimName string, statefulSetName string) error {
+	exists, err := kubectl.HasVolumeClaim(fs, volumeClaimName)
+	if err != nil {
+		return fmt.Errorf("checking for volume claim: %w", err)
+	}
+
+	if !exists {
+		return nil
+	}
+
+	zone, err := kubectl.GetVolumeClaimAZ(fs, volumeClaimName)
+	if err != nil {
+		return fmt.Errorf("acquiring volume claim AZ: %w", err)
+	}
+
+	err = kubectl.AddNodeSelector(fs, statefulSetName, kubectl.AvailabilityZoneLabelKey, zone)
+	if err != nil {
+		return fmt.Errorf("adding nodeSelector: %w", err)
 	}
 
 	return nil
