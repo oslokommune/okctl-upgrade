dfbc8f3f6da3103d74b31c851ae822df6a35f061
Julius Pedersen <deifyed@tutanota.com>
2022-05-20 10:02:06 +0200

    ✅ Add debug info and dry run to node selector functionality

--- upgrades/0.0.94.persist-loki/pkg/loki/api.go
+++ upgrades/0.0.94.persist-loki/pkg/loki/api.go
@@ -4,15 +4,13 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/spf13/afero"
-
 	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.94.persist-loki/pkg/lib/context"
 
 	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.94.persist-loki/pkg/kubectl"
 )
 
 func AddPersistence(ctx context.Context, region string, clusterName string, bucketName string) error {
-	err := ensureNodeSelector(ctx.Fs, "loki", "loki")
+	err := ensureNodeSelector(ctx, "loki", "loki")
 	if err != nil {
 		return fmt.Errorf("ensuring defined nodeSelector: %w", err)
 	}
@@ -64,22 +62,32 @@ func AddPersistence(ctx context.Context, region string, clusterName string, buck
 	return nil
 }
 
-func ensureNodeSelector(fs *afero.Afero, volumeClaimName string, statefulSetName string) error {
-	exists, err := kubectl.HasVolumeClaim(fs, volumeClaimName)
+func ensureNodeSelector(ctx context.Context, volumeClaimName string, statefulSetName string) error {
+	ctx.Logger.Debug("Checking for volume claim")
+
+	exists, err := kubectl.HasVolumeClaim(ctx.Fs, volumeClaimName)
 	if err != nil {
 		return fmt.Errorf("checking for volume claim: %w", err)
 	}
 
 	if !exists {
+		ctx.Logger.Debug("Volume claim not found, ignoring node selector")
+
 		return nil
 	}
 
-	zone, err := kubectl.GetVolumeClaimAZ(fs, volumeClaimName)
+	zone, err := kubectl.GetVolumeClaimAZ(ctx.Fs, volumeClaimName)
 	if err != nil {
 		return fmt.Errorf("acquiring volume claim AZ: %w", err)
 	}
 
-	err = kubectl.AddNodeSelector(fs, statefulSetName, kubectl.AvailabilityZoneLabelKey, zone)
+	ctx.Logger.Debug("Found volume AZ %s, adding node selector to loki", zone)
+
+	if ctx.Flags.DryRun {
+		return nil
+	}
+
+	err = kubectl.AddNodeSelector(ctx.Fs, statefulSetName, kubectl.AvailabilityZoneLabelKey, zone)
 	if err != nil {
 		return fmt.Errorf("adding nodeSelector: %w", err)
 	}
