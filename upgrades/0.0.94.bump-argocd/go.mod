47ddd77a5d434a8da48194ea917af1d84b0f3f2c
Julius Pedersen <deifyed@tutanota.com>
2022-05-27 08:48:02 +0200

    ✅ Scaffold upgrade

---
+++ upgrades/0.0.94.bump-argocd/go.mod
@@ -0,0 +1,8 @@
+module github.com/oslokommune/okctl-upgrade/upgrades/0.0.94.bump-argocd
+
+go 1.16
+
+require (
+	github.com/AlecAivazis/survey/v2 v2.3.2
+	github.com/spf13/cobra v1.2.1
+)
---
+++ upgrades/0.0.94.bump-argocd/upgrade.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.94.bump-argocd/pkg/lib/cmdflags"
+)
+
+func upgrade(context Context, flags cmdflags.Flags) error {
+	return nil
+}
