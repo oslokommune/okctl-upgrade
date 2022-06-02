a65fe5b7a20a8642907dad5e7b1a9dffe49beeb3
Julius Pedersen <deifyed@tutanota.com>
2022-06-02 08:52:18 +0200

    ✅ Add debug output

--- upgrades/0.0.96.remote-state-versioning/upgrade.go
+++ upgrades/0.0.96.remote-state-versioning/upgrade.go
@@ -5,26 +5,35 @@ import (
 	"fmt"
 	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.96.remote-state-versioning/pkg/cfn"
 	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.96.remote-state-versioning/pkg/lib/cmdflags"
+	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.96.remote-state-versioning/pkg/lib/logging"
 	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.96.remote-state-versioning/pkg/patch"
 )
 
-func upgrade(ctx context.Context, flags cmdflags.Flags) error {
+func upgrade(ctx context.Context, log logging.Logger, flags cmdflags.Flags) error {
 	stackName := stackName("julius-one")
+
+	log.Debugf("Fetching stack with name %s", stackName)
 
 	template, err := cfn.FetchStackTemplate(ctx, stackName)
 	if err != nil {
 		return fmt.Errorf("fetching template: %w", err)
 	}
 
+	log.Debug("Found stack. Starting patch operation")
+
 	patchedTemplate, err := patch.AddBucketVersioning(template)
 	if err != nil {
 		return fmt.Errorf("patching: %w", err)
 	}
 
+	log.Debug("Patching successful. Updating stack.")
+
 	err = cfn.UpdateStackTemplate(ctx, stackName, patchedTemplate)
 	if err != nil {
 		return fmt.Errorf("updating template: %w", err)
 	}
+
+	log.Debug("Update success.")
 
 	return nil
 }
