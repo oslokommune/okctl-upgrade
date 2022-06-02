2695bef28707b7c4923571a128afc615dc3c84ba
Julius Pedersen <deifyed@tutanota.com>
2022-06-02 08:34:32 +0200

    ✅ Add rough upgrade flow

--- upgrades/0.0.96.remote-state-versioning/upgrade.go
+++ upgrades/0.0.96.remote-state-versioning/upgrade.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.96.remote-state-versioning/pkg/cfn"
 	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.96.remote-state-versioning/pkg/lib/cmdflags"
-	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.96.remote-state-versioning/pkg/somecomponent"
+	"github.com/oslokommune/okctl-upgrade/upgrades/0.0.96.remote-state-versioning/pkg/patch"
 )
 
-func upgrade(context Context, flags cmdflags.Flags) error {
-	c := somecomponent.New(context.logger, flags)
+func upgrade(ctx context.Context, flags cmdflags.Flags) error {
+	stackName := stackName("julius-one")
 
-	err := c.Upgrade()
+	template, err := cfn.FetchStackTemplate(ctx, stackName)
 	if err != nil {
-		return err
+		return fmt.Errorf("fetching template: %w", err)
+	}
+
+	patchedTemplate, err := patch.AddBucketVersioning(template)
+	if err != nil {
+		return fmt.Errorf("patching: %w", err)
+	}
+
+	err = cfn.UpdateStackTemplate(ctx, stackName, patchedTemplate)
+	if err != nil {
+		return fmt.Errorf("updating template: %w", err)
 	}
 
 	return nil
+}
+
+func stackName(clusterName string) string {
+	return fmt.Sprintf("okctl-s3bucket-%s-okctl-%s-meta", clusterName, clusterName)
 }
